e7f11de4a60ee0084f6cccff8c9693347e8d2e26
Shivang Gupta <shivangg66@gmail.com>
2020-12-06 00:54:48 +0530

    Class based app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
-
-function App() {
-  return (
-    <div className="App">
-      <h1> Groomers</h1>
-    </div>
-  );
+import React, { Component } from 'react';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1> Groomers</h1>
+      </div>
+    );
+  }
 }
-
 export default App;
